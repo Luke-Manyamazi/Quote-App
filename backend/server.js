f514118293cc4485576a57473e2a13d8d1e71caa
Luke-Manyamazi <lukemanyamazi1@gmail.com>
2025-08-19 19:03:20 +0200

    added port handling code

--- backend/server.js
+++ backend/server.js
@@ -494,6 +494,5 @@ app.post("/api/quote", (req, res) => {
   res.status(201).json({ message: "Quote added successfully!", newQuote });
 });
 
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
